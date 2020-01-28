ad9f1e7bd2d0a6afd30236e128ffd3899b6b2d31
Oscar Sandvik <oscar.elmqvistsandvik@gmail.com>
2020-01-28 08:07:13 +0100

    Fixed a bug in sell and implemented a god mode trader.

--- my_trader.py
+++ my_trader.py
@@ -28,6 +28,17 @@ def get_one_stock_price(ticker, date):
     return price
 
 
+def god_mode(ticker):
+    prices = []  # ($)
+    with open(data_dir + ticker + '.csv', 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            date = row[0]
+            if date != 'Date':
+                prices.append(float(row[1][2:]))  # ($)
+    return prices
+
+
 def update_prices(date):
     ticker = 'AAPL'
     price = get_one_stock_price(ticker, date)
@@ -52,13 +63,18 @@ def buy_max(portfolio, ticker, ticker_price):
 
 
 def sell(portfolio, ticker, ticker_price, amount):
-    if amount < portfolio[ticker]:
+    if amount <= portfolio[ticker]:
         sell_value = ticker_price * amount  # ($)
         portfolio['Cash'] += sell_value  # ($)
         portfolio[ticker] -= amount
         return portfolio
     else:
         return portfolio
+
+
+def sell_max(portfolio, ticker, ticker_price):
+    max_amount = portfolio[ticker]
+    return sell(portfolio, ticker, ticker_price, max_amount)
 
 
 def update_net_worth(portfolio, prices):
@@ -69,6 +85,8 @@ def update_net_worth(portfolio, prices):
     portfolio['Net Worth'] = net_worth
     return portfolio
 
+god_mode_prices = god_mode('AAPL')  # ($)
+
 days_gone_by = 1  # warning 1 will mean the first date in dates
 while len(dates) >= days_gone_by:
     print(dates[-days_gone_by])
@@ -78,10 +96,20 @@ while len(dates) >= days_gone_by:
     print(prices)
 
     # trade
-    if holdings['AAPL'] > 5:
-        holdings = sell(holdings, 'AAPL', prices['AAPL'], 2)
-    elif holdings['Cash'] > prices['AAPL']:
-        holdings = buy_max(holdings, 'AAPL', prices['AAPL'])
+    if len(dates) > days_gone_by + 1:
+        future_arbitrage = god_mode_prices[-(days_gone_by + 1)] \
+                           - god_mode_prices[-days_gone_by]  # ($)
+        print('future_arbitrage: {}'.format(future_arbitrage))
+        if future_arbitrage < 0:
+            holdings = sell_max(holdings, 'AAPL', prices['AAPL'])
+        elif future_arbitrage > 0:
+            holdings = buy_max(holdings, 'AAPL', prices['AAPL'])
+
+
+    # if holdings['AAPL'] > 5:
+    #     holdings = sell_max(holdings, 'AAPL', prices['AAPL'])
+    # elif holdings['Cash'] > prices['AAPL']:
+    #     holdings = buy_max(holdings, 'AAPL', prices['AAPL'])
 
     # print wealth
     holdings = update_net_worth(holdings, prices)
