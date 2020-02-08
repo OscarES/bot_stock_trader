dc22bf370e1e26844193f7672178c1ac07bf7d3c
Oscar Sandvik <oscar.elmqvistsandvik@gmail.com>
2020-02-08 08:38:37 +0100

    Added a new easier way of calculating the arbitrages for all available tickers. Thus, profits reached new highs as I was able to only use the finest arbitrage for each day.

--- my_trader.py
+++ my_trader.py
@@ -2,12 +2,21 @@ import time
 import random
 import csv
 import math
+from os import listdir
+from os.path import isfile, join
+import operator
 
 data_dir = '/mnt/c/Users/oscar/Documents/Stocks/'
 
 delay = 0.5  # (s)
 
 holdings = {'Net Worth': 1000000,'Cash': 1000000, 'AAPL': 0}
+
+ticker_files = [f for f in listdir(data_dir) if isfile(join(data_dir, f))]
+tickers = [f[:-4] for f in ticker_files if f[-4:] == '.csv']
+
+for ticker in tickers:
+    holdings[ticker] = 0
 
 dates = []
 with open(data_dir + 'AAPL' + '.csv', 'r') as f:
@@ -22,28 +31,20 @@ with open(data_dir + 'AAPL' + '.csv', 'r') as f:
 def get_one_stock_price(ticker, date):
     with open(data_dir + ticker + '.csv', 'r') as f:
         reader = csv.reader(f)
+        price = 0  # ($)
         for row in reader:
             if row[0] == date:
                 price = float(row[1][2:])  # ($)
     return price
 
 
-def god_mode(ticker):
-    prices = []  # ($)
-    with open(data_dir + ticker + '.csv', 'r') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            date = row[0]
-            if date != 'Date':
-                prices.append(float(row[1][2:]))  # ($)
-    return prices
-
-
 def update_prices(date):
     ticker = 'AAPL'
-    price = get_one_stock_price(ticker, date)
-    # price = 10 + random.random() * 5  # ($)
-    updated_prices = {ticker: price}  # ($)
+    updated_prices = {}
+    for ticker in tickers:
+        price = get_one_stock_price(ticker, date)
+        # price = 10 + random.random() * 5  # ($)
+        updated_prices[ticker] = price  # ($)
     return updated_prices
 
 
@@ -58,6 +59,8 @@ def buy(portfolio, ticker, ticker_price, amount):
 
 
 def buy_max(portfolio, ticker, ticker_price):
+    if ticker_price == 0:
+        return portfolio
     max_amount = math.floor(portfolio['Cash'] / ticker_price)
     return buy(portfolio, ticker, ticker_price, max_amount)
 
@@ -77,6 +80,12 @@ def sell_max(portfolio, ticker, ticker_price):
     return sell(portfolio, ticker, ticker_price, max_amount)
 
 
+def sell_all(portfolio, prices):
+    for ticker in tickers:
+        portfolio = sell_max(portfolio, ticker, prices[ticker])
+    return portfolio
+
+
 def update_net_worth(portfolio, prices):
     net_worth = portfolio['Cash']  # ($)
     for ticker in prices:
@@ -85,25 +94,24 @@ def update_net_worth(portfolio, prices):
     portfolio['Net Worth'] = net_worth
     return portfolio
 
-god_mode_prices = god_mode('AAPL')  # ($)
 
 days_gone_by = 1  # warning 1 will mean the first date in dates
 while len(dates) >= days_gone_by:
     print(dates[-days_gone_by])
 
-    # load next day's opening values
+    # load next day's closing values
     prices = update_prices(dates[-days_gone_by])
     print(prices)
 
     # trade
     if len(dates) > days_gone_by + 1:
-        future_arbitrage = god_mode_prices[-(days_gone_by + 1)] \
-                           - god_mode_prices[-days_gone_by]  # ($)
-        print('future_arbitrage: {}'.format(future_arbitrage))
-        if future_arbitrage < 0:
-            holdings = sell_max(holdings, 'AAPL', prices['AAPL'])
-        elif future_arbitrage > 0:
-            holdings = buy_max(holdings, 'AAPL', prices['AAPL'])
+        tomorrows_prices = update_prices(dates[-(days_gone_by + 1)])
+        arbitrages = {k : tomorrows_prices[k] - prices[k] for k in prices.keys()}
+        holdings = sell_all(holdings, prices)
+        best_arbitrage_ticker = max(arbitrages.items(), key=operator.itemgetter(1))[0]
+        # print('Arbitrages: {}'.format(arbitrages))
+        if arbitrages[best_arbitrage_ticker] > 0:
+            holdings = buy_max(holdings, best_arbitrage_ticker, prices[best_arbitrage_ticker])
 
 
     # if holdings['AAPL'] > 5:
